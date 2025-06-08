5c951f0ce2b4e17abe52b1ef62109e62e0f8055c
davidkeo2398 <nguyenboiky@gmail.com>
2025-06-08 17:44:33 +0700

    BE-migration: [UPDATED] add get product detail and get list products by category

--- backend/src/services/product-service.js
+++ backend/src/services/product-service.js
@@ -11,5 +11,23 @@ module.exports = {
         catch (err) {
             throw new Error('Get products failure: ', err)
         }
+    },
+    getProductById: async (product_id) => {
+        try {
+            const product = Product.findOne({ where: { product_id: product_id } });
+            return product;
+        }
+        catch (err) {
+            throw new Error('Get a product fail: ', err)
+        }
+    },
+    getProductByCategory: async (category_id) => {
+        try {
+            const products = Product.findAll({ where: { category_id: category_id } });
+            return products;
+        } catch (err) {
+            throw new Error('Get products by category fail: ', err)
+
+        }
     }
 }
