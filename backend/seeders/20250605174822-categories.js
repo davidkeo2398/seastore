57e49b0ce627447b93fb478ef2a96db62b5e9ce8
davidkeo2398 <nguyenboiky@gmail.com>
2025-06-08 14:17:01 +0700

    BE-migration: [UPDATED] data for categories and products

--- backend/seeders/20250605174822-categories.js
+++ backend/seeders/20250605174822-categories.js
@@ -1,25 +1,29 @@
 'use strict';
 
 const { faker } = require('@faker-js/faker');
+const path = require('path');
+const fs = require('fs');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     await queryInterface.sequelize.query('SET FOREIGN_KEY_CHECKS = 0');
-    const categories = [];
-    for (let i = 0; i < 10; i++) {
-      categories.push({
-        category_name: faker.commerce.department(),
-        description: faker.lorem.sentence(),
-        createdAt: new Date(),
-        updatedAt: new Date()
-      });
-    }
+    const categoriesPath = path.join(__dirname, '../JSON/categories.js');
+    const categoriesData = fs.readFileSync(categoriesPath, 'utf8');
+    const categories = JSON.parse(categoriesData);
+    // for (let i = 0; i < 10; i++) {
+    //   categories.push({
+    //     category_name: faker.commerce.department(),
+    //     description: faker.lorem.sentence(),
+    //     createdAt: new Date(),
+    //     updatedAt: new Date()
+    //   });
+    // }
     await queryInterface.bulkInsert('categories', categories);
     await queryInterface.sequelize.query('SET FOREIGN_KEY_CHECKS = 1');
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     await queryInterface.sequelize.query('SET FOREIGN_KEY_CHECKS = 0');
     await queryInterface.bulkDelete('categories', null, {});
     await queryInterface.sequelize.query('ALTER TABLE categories AUTO_INCREMENT = 1;');
